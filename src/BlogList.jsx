7bb0afac304c60e55a8f797bb2c1339cc969da62
khaledxyz <attano001@outlook.com>
2022-05-16 16:50:20 +0100

    Created fake json server, and loading screen.

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,19 +1,17 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
-
+const BlogList = ({blogs, title}) => {
+    // const {blogs, title} = props;
   return (
     <div className="blog-list">
         <h2>{ title }</h2>
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          {console.log(blog)}
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
+        {blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <button>Delete</button>
+          </div>
       ))}
     </div>
   );
 };
-
+ 
 export default BlogList;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            id: 1,
-            title: "Blog 1",
-            body: "This is blog 1",
-            author: "Khaled" 
-        },
-        {
-            id: 2,
-            title: "Blog 2",
-            body: "This is blog 2",
-            author: "Yoshi"
-        },
-        {
-            id: 3,
-            title: "Blog 3",
-            body: "This is blog 3",
-            author: "Mario"
-        },
-        {
-            id: 4,
-            title: "Blog 4",
-            body: "This is blog 4",
-            author: "Mario"
-        },
-    ])
+    const [blogs, setBlogs] = useState(null); 
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        fetch('http://localhost:800/blogs')
+        .then((resposne) => resposne.json())
+        .then((data) => {setBlogs(data); setIsLoading(false);})
+    }, [])
+
+    const template = (
+        <div className="loader">
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+            <div className="loader-square"></div>
+        </div>
+    )
     return ( 
+        
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!"/>
-            <BlogList blogs={blogs.filter(blog => blog.author === 'Mario')} title="Mario's Blogs!"/>
+            {/* first "blogs" is the prop name, {blogs} is the data we pass in */}
+            {isLoading && template}
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
      );
 }
